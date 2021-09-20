2ffe4df241444956e4fc1c0dd42087f875f3bd20
arnaldoa13 <87203149+arnaldoa13@users.noreply.github.com>
2021-09-20 15:38:49 +0100

    Add files via upload
    Dicreased wheel radius

--- dev/inc/functions.php
+++ dev/inc/functions.php
@@ -1,7 +1,7 @@
 <?php 
 
 function radialProgress($per){
-	$radius=150;
+	$radius=105;
 	if($per){
 		$percent = intval($per);		
 	}else{
@@ -44,5 +44,63 @@ function radialProgress($per){
 ";
 	return $radial;
 }
+
+$states = array(
+	'AL'=>'ALABAMA',
+	'AK'=>'ALASKA',
+	'AZ'=>'ARIZONA',
+	'AR'=>'ARKANSAS',
+	'CA'=>'CALIFORNIA',
+	'CO'=>'COLORADO',
+	'CT'=>'CONNECTICUT',
+	'DE'=>'DELAWARE',
+	'FL'=>'FLORIDA',
+	'GA'=>'GEORGIA',
+	'GU'=>'GUAM GU',
+	'HI'=>'HAWAII',
+	'ID'=>'IDAHO',
+	'IL'=>'ILLINOIS',
+	'IN'=>'INDIANA',
+	'IA'=>'IOWA',
+	'KS'=>'KANSAS',
+	'KY'=>'KENTUCKY',
+	'LA'=>'LOUISIANA',
+	'ME'=>'MAINE',
+	'MH'=>'MARSHALL ISLANDS',
+	'MD'=>'MARYLAND',
+	'MA'=>'MASSACHUSETTS',
+	'MI'=>'MICHIGAN',
+	'MN'=>'MINNESOTA',
+	'MS'=>'MISSISSIPPI',
+	'MO'=>'MISSOURI',
+	'MT'=>'MONTANA',
+	'NE'=>'NEBRASKA',
+	'NV'=>'NEVADA',
+	'NH'=>'NEW HAMPSHIRE',
+	'NJ'=>'NEW JERSEY',
+	'NM'=>'NEW MEXICO',
+	'NY'=>'NEW YORK',
+	'NC'=>'NORTH CAROLINA',
+	'ND'=>'NORTH DAKOTA',
+	'OH'=>'OHIO',
+	'OK'=>'OKLAHOMA',
+	'OR'=>'OREGON',
+	'PW'=>'PALAU',
+	'PA'=>'PENNSYLVANIA',
+	'PR'=>'PUERTO RICO',
+	'RI'=>'RHODE ISLAND',
+	'SC'=>'SOUTH CAROLINA',
+	'SD'=>'SOUTH DAKOTA',
+	'TN'=>'TENNESSEE',
+	'TX'=>'TEXAS',
+	'UT'=>'UTAH',
+	'VT'=>'VERMONT',
+	'VI'=>'VIRGIN ISLANDS',
+	'VA'=>'VIRGINIA',
+	'WA'=>'WASHINGTON',
+	'WV'=>'WEST VIRGINIA',
+	'WI'=>'WISCONSIN',
+	'WY'=>'WYOMING',
+);
 
  ?>
