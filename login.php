9373eca801a05333e5079ca3fcf2a6cf940967ec
arnaldoa13 <87203149+arnaldoa13@users.noreply.github.com>
2021-09-20 15:06:47 +0100

    Add files via upload
    Login screen by Bryan

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php include 'inc/header.php'; ?>
+
+<div class="main-container pattern-bg-dark mobile login-screen">
+	<div class="login-container">
+		<img class="logo" src="images/logo.svg">
+		<form class="login" method="" action="">
+			<input type="email" name="email" placeholder="Email">
+			<input type="password" name="pass" placeholder="Password"><br>
+			<select>
+				<option value="0" disabled="true">Market</option>
+				<option value="0">Market01</option>
+				<option value="0">Market01</option>
+				<option value="0">Market01</option>
+				<option value="0">Market01</option>
+				<option value="0">Market01</option>
+				<option value="0">Market01</option>
+				<option value="0">Market01</option>
+				<option value="0">Market01</option>
+			</select>
+			<button type="submit" class="aa-btn blue caps">SIGN IN</button>
+		</form>
+			<a href="#">Create an account</a>
+			<a href="#">Need help?</a>
+	</div>	
+</div>
+<?php include 'inc/footer.php'; ?>
