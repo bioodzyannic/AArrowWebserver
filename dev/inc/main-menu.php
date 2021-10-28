db1ed79a2db56cb1b06c8ac0fb60b241865bb996
arnaldoa13 <87203149+arnaldoa13@users.noreply.github.com>
2021-10-28 17:55:18 +0100

    Update main-menu.php

--- dev/inc/main-menu.php
+++ dev/inc/main-menu.php
@@ -9,13 +9,14 @@
 			</ul>
 		</div>
 		<div class="user-options">
-			<div></div>
-			<div></div>
-			<div></div>
+			<div class="user-option apps" data-open="0" onclick="this.setAttribute('data-open', this.getAttribute('data-open') === '1' ? '0' : '1');">
+				<img src="../images/apps.svg" class="">
+				<div class="apps-container">
+					
+				</div>
+			</div>
+			<div class="user-option" data-open="0" onclick="this.setAttribute('data-open', this.getAttribute('data-open') === '1' ? '0' : '1');">
+				<img src="../images/gear.svg" class="">
+			</div>
 		</div>
 	</div>
-	
-		
-	
-	
-
