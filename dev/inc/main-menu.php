45ba6cc919d36d66bff2fe25bc56fb8367bb3dea
arnaldoa13 <87203149+arnaldoa13@users.noreply.github.com>
2021-12-16 18:10:19 +0000

    Add files via upload

--- dev/inc/main-menu.php
+++ dev/inc/main-menu.php
@@ -6,14 +6,17 @@
 				</a>
 			</div>
 			<ul class="menu-options">
-				<li><a href="/desktop/index.php">Home</a></li>
+				<li><a href="/desktop/index.php">Mission control</a></li>
 				
 			</ul>
 		</div>
 		<div class="user-options">
 			<div class="user-option apps" data-open="0" onclick="this.setAttribute('data-open', this.getAttribute('data-open') === '1' ? '0' : '1');">
 				<img src="../images/apps.svg" class="">
-				<div class="apps-container">
+				<div class="apps-container" onclick="event.stopPropagation()">
+					<div><a href="#"><img src="https://aarrowsignspinners.com/wp-content/uploads/2020/12/hricon_wh3.png" alt=""></a></div>
+					<div><a href="#"><img src="https://aarrowsignspinners.com/wp-content/uploads/2020/12/hricon_wh3.png" alt=""></a></div>
+					<div><a href="#"><img src="https://aarrowsignspinners.com/wp-content/uploads/2020/12/hricon_wh3.png" alt=""></a></div>
 					
 				</div>
 			</div>
