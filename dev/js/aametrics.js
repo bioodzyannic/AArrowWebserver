f1b5a2b4949aee6bf3a2929221bee10cecb67141
arnaldoa13 <87203149+arnaldoa13@users.noreply.github.com>
2022-04-14 15:53:26 +0100

    Add files via upload

--- dev/js/aametrics.js
+++ dev/js/aametrics.js
@@ -3,19 +3,75 @@ class AAMetrics{
 		this.json = JSON.parse(json);
 		this.time_sort = time_sort;
 		this.metrics = {
-			totalHours:{color:"rgb(3, 127, 242, 1)",label:"Schd Hours"},
-			workedHours:{color:"rgba(0, 219, 246, 1)",label:"Worked Hours"},
-			missedHours:{color:"rgba(255, 0, 0, 1)",label:"Missed Hours"},
-			numberOfShifts:{color:"rgba(7, 57, 145, 1)",label:"Shifts"},
-			revenue:{color:"rgba(4, 179, 50, 1)",label:"Revenue"},
-			filledRate:{color:"rgba(0, 150, 136, 1)",label:"Fill Rate"}
+			totalHours:{color:"var(--selected,#12bd1c)",label:"Schd Hours"},
+			workedHours:{color:"var(--selected,#12bd1c)",label:"Worked Hours"},
+			missedHours:{color:"var(--selected,#12bd1c)",label:"Missed Hours"},
+			numberOfShifts:{color:"var(--selected,#12bd1c)",label:"Shifts"},
+			revenue:{color:"var(--selected,#12bd1c)",label:"Revenue"},
+			filledRate:{color:"var(--selected,#12bd1c)",label:"Fill Rate"}
 		};
 		// this.selectedMetrics = ["totalHours","workedHours","missedHours","numberOfShifts","revenue"];
 		this.selectedMetrics = ["totalHours"];
+		this.regions = this.getRegions();
+        this.regionsColors={}
+		this.selectedRegions = [Object.keys(this.getRegions())[0]];
 		this.dateOptions = { weekday: 'long', year: 'numeric', month: 'numeric', day: 'numeric' };
 		this.ranges = ['day','week','month','year'];
 		this.types = ['line','bar','doughnut'];
 	}
+	// -----------------------------------------------
+	// Markets
+	// -----------------------------------------------
+     
+    getRegions(key='region'){
+        return this.json.reduce((group,elem)=>{
+            let keyValue = elem[key]
+            return {...group,[keyValue] : [...(group[keyValue] ?? []), elem]}
+        },{});
+    }
+
+    regionColor(factor){
+        return `hsl(${360 + (factor * 15)}deg 78% 52%)`
+    }
+
+    regionSelectors(){
+		let target = document.querySelector('#global-regions'); 
+		Object.keys(this.getRegions()).forEach((t,index) => {
+			let div = document.createElement('DIV');
+			div.setAttribute('onclick','select(this)');
+			div.className = this.selectedRegions[0] == t ? 'event range-selector active' : 'event range-selector'; 
+			div.innerHTML = `<span class="value">${this.regions[t][0].marketName}</span>`;
+
+            //COLORS
+            
+            let regionColor = this.regionColor(index);
+            this.regionsColors[t] = regionColor;
+			div.style = `--selected:${regionColor}`;
+			div.addEventListener('click',()=>{
+				this.changeSelectedRegion(t);
+				// Y
+				let newY = this.gatherYDataset();
+				this._graph.yDataset.data = newY.data;
+				this._graph._chart.data.datasets = newY.data;
+				// Y scale
+				this._graph._chart.options.scales.y.max = newY.max * 1.1
+
+				this._graph._chart.update();
+				
+			});
+			target.append(div)
+		});
+	}
+
+    changeSelectedRegion(newRegion){
+		if(!this.selectedRegions.includes(newRegion)){
+			this.selectedRegions.push(newRegion);
+		}else{
+			this.selectedRegions = this.selectedRegions.filter(m => m !== newRegion);
+		}
+        this.update();
+	}
+
 
 	// -----------------------------------------------
 	// GLOBALS METRICS
@@ -31,6 +87,7 @@ class AAMetrics{
 		];
 
 		let target = document.querySelector('#global-info');
+        target.innerHTML='';
 		metrics.forEach(m => {
 			let p = document.createElement('P');
 			p.innerHTML = `<b>${m.name}</b> ${m.data}`;
@@ -40,44 +97,56 @@ class AAMetrics{
 
 	globalTotalHours(){
 		let totalHours=0;
-		this.json.forEach(shift => {
-			totalHours += this.totalHours(shift);
-		});
+        this.selectedRegions.map((region,i)=>{ 
+            this.regions[region].forEach(shift => {
+                totalHours += this.totalHours(shift);
+            });
+        });
 		return {name: 'Total Schd Hours:',data: totalHours};
 	}
 
 	globalTotalRevenue(){
 		let revenue=0;
-		this.json.forEach(shift => {
+        this.selectedRegions.map((region,i)=>{ 
+            this.regions[region].forEach(shift => {
 			revenue += this.revenue(shift);
+            });
 		});
 		return {name: 'Total Revenue: $',data: revenue};
 	}
 
 	globalTotalWorkedHours(){
 		let totalHours=0;
-		this.json.forEach(shift => {
-			totalHours += this.workedHours(shift);
+        this.selectedRegions.map((region,i)=>{ 
+            this.regions[region].forEach(shift => {
+                totalHours += this.workedHours(shift);
+            });
 		});
 		return {name: 'Total Worked Hours:',data: totalHours};
 	}
 	globalMissedWorkedHours(){
 		let totalHours=0;
-		this.json.forEach(shift => {
-			totalHours += this.missedHours(shift);
+        this.selectedRegions.map((region,i)=>{ 
+            this.regions[region].forEach(shift => {
+                totalHours += this.missedHours(shift);
+            });
 		});
 		return {name: 'Total Missed Hours:',data: totalHours};
 	}
 	
 	globalTotalShifts(){
-		return {name: 'Total Shifts:',data: this.json.length};
+        let shifts=0;
+        this.selectedRegions.map((region,i)=>{ 
+            this.regions[region].forEach(shift => {
+                shifts += 1;
+            });
+		});
+		return {name: 'Total Shifts:',data: shifts};
 	}
 
 	changeSelectedMetric(newMetric){
 		if(!this.selectedMetrics.includes(newMetric)){
-			this.selectedMetrics.push(newMetric);
-		}else{
-			this.selectedMetrics = this.selectedMetrics.filter(m => m !== newMetric);
+			this.selectedMetrics = [newMetric];
 		}
 	}
 
@@ -85,7 +154,7 @@ class AAMetrics{
 		let target = document.querySelector('#global-metric'); 
 		Object.keys(this.metrics).forEach((t,index) => {
 			let div = document.createElement('DIV');
-			div.setAttribute('onclick','select(this)');
+			div.setAttribute('onclick','select(this,true)');
 			div.className = this.selectedMetrics[0] == t ? 'event range-selector active' : 'event range-selector'; 
 			div.innerHTML = `<span class="value">${this.metrics[t].label}</span>`;
 			div.style = `--selected:${this.metrics[t].color}`;
@@ -96,7 +165,7 @@ class AAMetrics{
 				this._graph.yDataset.data = newY.data;
 				this._graph._chart.data.datasets = newY.data;
 				// Y scale
-				this._graph._chart.options.scales.y.max = newY.max + 1
+				this._graph._chart.options.scales.y.max = newY.max * 1.1
 
 				this._graph._chart.update();
 				
@@ -118,8 +187,6 @@ class AAMetrics{
 			div.innerHTML = `<span class="value">${t.toUpperCase()}</span>`;
 			div.addEventListener('click',()=>{
 				this._graph.changeType(t)
-				// select(this,true)
-				// changeDateRange
 			});
 			target.append(div)
 		});
@@ -254,8 +321,11 @@ class AAMetrics{
 		this._graph.yDataset.data = newY.data;
 		this._graph._chart.data.datasets = newY.data;
 		// Y scale
-		this._graph._chart.options.scales.y.max = newY.max + 1
+		this._graph._chart.options.scales.y.max = newY.max * 1.1
         this._graph._chart.update();
+
+        this.globalMetrics();
+        
     }
 	
 	// -----------------------------------------------
@@ -268,7 +338,9 @@ class AAMetrics{
 	}
 
 	revenue(shift){
-		return shift.revenue;
+        let r = parseFloat(shift.revenue.replace('$',''));
+        // console.log(r);
+		return r;
 	}
 
 	totalHours(shift){
@@ -282,16 +354,16 @@ class AAMetrics{
 	missedHours(shift){
 		let workable = this.totalHours(shift);
 		let worked = this.workedHours(shift);
-		console.log(shift.date);
-		console.log("Workable",workable);
-		console.log("Worked",worked);
+		// console.log(shift.date);
+		// console.log("Workable",workable);
+		// console.log("Worked",worked);
 		if(workable - worked <= 0){
-			console.log("Missed", 0);
-			console.log('-------------------');
+			// console.log("Missed", 0);
+			// console.log('-------------------');
 			return 0
 		}else{
-			console.log("Missed", workable - worked);
-			console.log('-------------------');
+			// console.log("Missed", workable - worked);
+			// console.log('-------------------');
 			return workable - worked
 		}
 	}
@@ -301,58 +373,76 @@ class AAMetrics{
 	// 	return (worked * 100)/ workable;
 	// }
 
-	yAxis(){
-		let y = this.selectedMetrics.map(metric => {
-			let values = this.xAxis().data.map(x => {
-				var yValue = 0;
-				this.json.forEach((y,index) => {
-					if(this.time_sort=="day"){
-						// DAILY
-						if(x.getTime() == new Date(y.date).getTime()){
-							yValue+=eval(`this.${metric}(${JSON.stringify(y)})`);
-						}
-					}else if(this.time_sort=="week"){
-						//WEEKLY
-						let next = this.json[index+1] ? new Date(this.json[index+1].date) : new Date(this.json[index].date);
-						if(x.getTime() == new Date(y.date).getTime() || x.getTime() < next.getTime() && new Date(y.date).getTime() - x.getTime() < 6.048e8){
-							yValue+=eval(`this.${metric}(${JSON.stringify(y)})`);
 
-						}
-					}else if(this.time_sort=="month"){
-						//Monthly					
-						if(new Date(y.date).getMonth() == x.getMonth()){
-							yValue+=eval(`this.${metric}(${JSON.stringify(y)})`);
-						}
-					}else if(this.time_sort=="year"){
-						//Monthly					
-						if(new Date(y.date).getFullYear() == x.getFullYear()){
-							yValue+=eval(`this.${metric}(${JSON.stringify(y)})`);
-						}
-					}
-			});
-			return yValue;
-		});
-		return {name:metric,data:values,color:this.metrics[metric].color,label:this.metrics[metric].label};
-		});
-		return y;
+	yAxis(){
+        let regionsY = this.selectedRegions.map((region,i)=>{
+            let regionData = this.regions[region];
+            let y = this.selectedMetrics.map(metric => {
+                let values = this.xAxis().data.map(x => {
+                    var yValue = 0;
+                    regionData.forEach((y,index) => {
+                        if(this.time_sort=="day"){
+                            // DAILY
+                            if(x.getUTCDate() == new Date(y.date).getUTCDate()){
+                                yValue+=eval(`this.${metric}(${JSON.stringify(y)})`);
+                            }
+                        }else if(this.time_sort=="week"){
+                            //WEEKLY
+                            let next = this.json[index+1] ? new Date(this.json[index+1].date) : new Date(this.json[index].date);
+                            if(x.getTime() == new Date(y.date).getTime() || x.getTime() < next.getTime() && new Date(y.date).getTime() - x.getTime() < 6.048e8){
+                                yValue+=eval(`this.${metric}(${JSON.stringify(y)})`);
+
+                            }
+                        }else if(this.time_sort=="month"){
+                            //Monthly					
+                            if(new Date(y.date).getMonth() == x.getMonth()){
+                                yValue+=eval(`this.${metric}(${JSON.stringify(y)})`);
+                            }
+                        }else if(this.time_sort=="year"){
+                            //Monthly					
+                            if(new Date(y.date).getFullYear() == x.getFullYear()){
+                                yValue+=eval(`this.${metric}(${JSON.stringify(y)})`);
+                            }
+                        }
+                });
+                return yValue;
+            });
+            console.log(this.regionsColors[region]);
+            return {region:region[0],name:metric,data:values,color:this.regionsColors[region],label:this.metrics[metric].label};
+            });
+            return y;
+        });
+        return regionsY;
 	}
 
 	gatherYDataset(){
 		const yAxis = this.yAxis();
 		let maxYValue = 0;
-		const yArray = yAxis.map((y,index) => {
-			console.log(y);
-			if(maxYValue < Math.max(...y.data)){maxYValue=Math.max(...y.data)}
-			return {
-					label: y.label,
-					data: y.data,
-					backgroundColor: y.color.replace("1)","0.5)"),
-					borderColor: y.color,
-					borderWidth: 2
-				}
+		const yArray = []
+        yAxis.forEach((yb,index) => {
+            yb.forEach(y=>{
+                if(maxYValue < Math.max(...y.data)){maxYValue=Math.max(...y.data)}
+                let bgc = y.color.split(' ');
+                console.log( `${bgc[0]} ${bgc[1]} 0%)`);
+                yArray.push(
+                        {
+                            label: y.label,
+                            data: y.data,
+                            backgroundColor: `${bgc[0]} ${bgc[1]} 90%)`,
+                            borderColor: y.color,
+                            borderWidth: 2
+                        }
+                    ) 
+                })
+			console.log(yb);
+
+            return yb;
 		});
+		console.log(yArray);
 		return {data:yArray,label:"",max:maxYValue};
 	}
+
+
 
 	// -----------------------------------------------
 	// RENDER
@@ -370,6 +460,9 @@ class AAMetrics{
 
 		// METRIC SELECTORS
 		this.metricSelectors();
+
+        // REGION SELECTORS
+        this.regionSelectors()
 
 		let y = this.gatherYDataset()
 
@@ -402,13 +495,6 @@ class AAGraph{
 	}
 
 	addYData(data){
-		// {
-		// 	label: 'Test',
-		// 	data: [8, 5, 1, 15, 9, 7,2],
-		// 	backgroundColor: 'rgba(54, 162, 235, 0.2)',
-		// 	borderColor: 'rgba(54, 162, 235, 1)',
-		// 	borderWidth: 1
-		// }
 		this.data.datasets.push(data)
 		this.update();
 	}
@@ -444,13 +530,18 @@ class AAGraph{
 						text: this.yDataset.label
 						},
 						min: 0,
-						max: this.yDataset.max + 1,
+						max: this.yDataset.max * 1.1,
 						ticks: {
 						// forces step size to be 50 units
 						stepSize: 1
 						}
 					}
-					}
+                },
+                plugins: {
+                    legend: {
+                        display: false,
+                    }
+                }
 				
 			}
 		});
